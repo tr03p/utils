b1385ece964b503867f2d9a503d168b24a3c114b
Hendrik Krüger <hendrik.kriiger@gmail.com>
2020-04-29 16:02:40 +0200

    Add prepend separator and remove crc32

--- crc32_filename.sh
+++ crc32_filename.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 DRY=0
+SEPARATOR=''
+REMOVE=0
 
 usage() {
   echo "Append the CRC32 to filename of each file in a directory"
@@ -8,6 +10,9 @@ usage() {
   echo "usage: $0 [-hn] directory_path"
   echo "  -h, --help    print out this help"
   echo "  -n, --dry     dry run, do not rename files"
+  echo "  -r            remove crc32"
+  echo "  -s            prepend space"
+  echo "  --sep char    prepend character"
   exit 0
 }
 
@@ -22,6 +27,19 @@ do
       
     -n | --dry)
       DRY=1
+      shift
+      ;;
+    -r)
+      REMOVE=1
+      shift
+      ;;
+    -s)
+      SEPARATOR=' '
+      shift
+      ;;
+    --sep)
+      SEPARATOR="$2"
+      shift
       shift
       ;;
 
@@ -46,19 +64,29 @@ then
   exit 1
 fi
 
+if [ $REMOVE = 1 ]; then
+  for FILENAME in "$1"/*; do
+    NEW_FILENAME=$(echo "$FILENAME" | sed -r "s/$SEPARATOR\[[A-F0-9]{8}\]//")
+    echo $NEW_FILENAME
+    if [ $DRY = 0 ]; then
+      mv "$FILENAME" "$NEW_FILENAME"
+    fi
+  done
+  exit 0
+fi
+
 for FILENAME in "$1"/*; do
   if [ -d "$FILENAME" ]; then
     continue
   fi
   HASH=$(crc32 "$FILENAME")
   #HASH=${HASH^^} # Bashism
-  HASH=`echo $HASH | tr '[a-z]' '[A-Z]'` # Uppercase
+  HASH=`echo $HASH | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]'` # Uppercase
   if [ ${#HASH} -ne 8 ]; then
     continue
   fi
-  
   TYPE=${FILENAME##*.}
-  NEW_FILENAME=${FILENAME%.$TYPE}[$HASH].$TYPE # Append CRC32 to filename
+  NEW_FILENAME=${FILENAME%.$TYPE}$SEPARATOR[$HASH].$TYPE # Append CRC32 to filename
   echo $NEW_FILENAME
   if [ $DRY = 0 ]; then
     mv "$FILENAME" "$NEW_FILENAME"
